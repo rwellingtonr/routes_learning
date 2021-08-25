9d92fd00a0447dd1583c92280bef5df51473663b
rwellingtonr <wellington-158@hotmail.com>
2021-08-24 23:46:27 -0300

    added routes to app.js

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/getInTouch">get in touch</a>
+        </li>
+        <li>
+          <a href="/support">Support</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/getInTouch.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const GetInTouch = () => {
+  return (
+    <div>
+      <h1>Get in Touch</h1>
+    </div>
+  )
+}
+export default GetInTouch
